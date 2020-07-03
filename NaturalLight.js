51d8d91d5fc0966601a62cb10b563a98c3d4f179
MelinaWa <43846707+MelinaWa@users.noreply.github.com>
2020-07-04 00:24:28 +0200

    test sp

--- NaturalLight.js
+++ NaturalLight.js
@@ -8,14 +8,5 @@ $(document).ready(function() {
 $("#roundslider1").roundSlider({
     sliderType: "default",
     value: 50
-});
 
-$("#roundslider2").roundSlider({
-    sliderType: "min-range",
-    value: 50
-});
-
-$("#roundslider3").roundSlider({
-    sliderType: "range",
-    value: "20,50"
 });
